732c02b4087cdc2824407793c37d010ae5c8155d
Grace Todd <gtodd04@qub.ac.uk>
2020-04-20 23:00:23 +0100

    Update Random.py

--- Random.py
+++ Random.py
@@ -3,21 +3,29 @@ import random
 player_pokemon = random.randint(1, 151)
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon)
 response = requests.get(url)
-pokemon = response.json()
-stat_dic = pokemon['stats']
+p_pokemon = response.json()
+stat_dic = p_pokemon['stats']
 speed = stat_dic[int('0')]
+hp = stat_dic[int('5')]
 print("Player Pokemon")
-print(pokemon['name'])
-print(pokemon['height'])
-print(pokemon['weight'])
+print(p_pokemon['name'])
+print(p_pokemon['height'])
+print(p_pokemon['weight'])
 print(speed['base_stat'])
+print(hp['base_stat'])
 
 computer_pokemon = random.randint(1, 151)
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(computer_pokemon)
 response = requests.get(url)
-pokemon = response.json()
+c_pokemon = response.json()
+c_pokemon = response.json()
+stat_dic = c_pokemon['stats']
+speed = stat_dic[int('0')]
+hp = stat_dic[int('5')]
 print(" ")
 print("Computer Pokemon")
-print(pokemon['name'])
-print(pokemon['height'])
-print(pokemon['weight'])
+print(c_pokemon['name'])
+print(c_pokemon['height'])
+print(c_pokemon['weight'])
+print(speed['base_stat'])
+print(hp['base_stat'])
