98b6077bfdee3376f7f48b72d5ba6cf8a8eb924f
Grace Todd <gtodd04@qub.ac.uk>
2020-04-20 22:53:06 +0100

    Update Random.py
    i am speed

--- Random.py
+++ Random.py
@@ -4,10 +4,13 @@ player_pokemon = random.randint(1, 151)
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon)
 response = requests.get(url)
 pokemon = response.json()
+stat_dic = pokemon['stats']
+speed = stat_dic[int('0')]
 print("Player Pokemon")
 print(pokemon['name'])
 print(pokemon['height'])
 print(pokemon['weight'])
+print(speed['base_stat'])
 
 computer_pokemon = random.randint(1, 151)
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(computer_pokemon)
