bfb13d66f6fbf3c66f3abc13ddd8fcf56f096075
Grace Todd <gtodd04@qub.ac.uk>
2020-04-20 22:04:30 +0100

    Update Random.py
    import pokemon api

--- Random.py
+++ Random.py
@@ -1,3 +1,8 @@
-print("hello, world!")
-print("hello, world - again!")
-print("this is a test")
+import requests
+from pprint import pprint
+pokemon_number = input("What is the Pokemon's ID? ")
+url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+response = requests.get(url)
+print(response)
+pokemon = response.json()
+pprint(pokemon)
