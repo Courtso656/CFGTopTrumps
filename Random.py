cb86de7e90aafc9e930e957470f64fa96e354363
Grace Todd <gtodd04@qub.ac.uk>
2020-04-24 17:43:33 +0100

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,9 +1,14 @@
 import requests
 import random
+print("Welcome to Top Dex!")
+print(" -Single Round")
+print(" -Multiple Rounds")
+print(" -Custom Game")
+print(" -Info")
 format = input("Which game type?" )
 if format == "Single Round":
     print("Loading new game")
-    restart = input("Ready to start?" )
+    restart = "yes"
     while restart == "yes" and format == "Single Round":
         player_pokemon = random.randint(1, 151)
         url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon)
@@ -88,6 +93,131 @@ if format == "Single Round":
                 print(other_height)
                 print("Your pokemon fainted")
                 restart = input("Want to play again?")
+        else:
+            print("Error")
+    else:
+        print("Thank you playing")
+elif format == "Custom Game":
+    print("Loading new game")
+    restart = "yes"
+    while restart == "yes" and format == "Custom Game":
+        pokedex= []
+        player_pokemon1 = input("What pokemon would you like in slot 1? (1-151)" )
+        url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon1)
+        response = requests.get(url)
+        p_pokemon = response.json()
+        pokedex.append(p_pokemon['name'])
+        player_pokemon1 = input("What pokemon would you like in slot 2? (1-151)" )
+        url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon1)
+        response = requests.get(url)
+        p_pokemon = response.json()
+        pokedex.append(p_pokemon['name'])
+        player_pokemon1 = input("What pokemon would you like in slot 3? (1-151)" )
+        url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon1)
+        response = requests.get(url)
+        p_pokemon = response.json()
+        pokedex.append(p_pokemon['name'])
+        player_pokemon1 = input("What pokemon would you like in slot 4? (1-151)" )
+        url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon1)
+        response = requests.get(url)
+        p_pokemon = response.json()
+        pokedex.append(p_pokemon['name'])
+        player_pokemon1 = input("What pokemon would you like in slot 5? (1-151)" )
+        url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon1)
+        response = requests.get(url)
+        p_pokemon = response.json()
+        pokedex.append(p_pokemon['name'])
+        player_pokemon1 = input("What pokemon would you like in slot 6? (1-151)" )
+        url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player_pokemon1)
+        response = requests.get(url)
+        p_pokemon = response.json()
+        pokedex.append(p_pokemon['name'])
+        print( )
+        print("Here's your pokemon team!")
+        print(pokedex)
+        print( )
+
+        response = requests.get(url)
+        p_pokemon = response.json()
+        stat_dic = p_pokemon['stats']
+        speed = stat_dic[int('0')]
+        hp = stat_dic[int('5')]
+        self_speed = speed['base_stat']
+        self_hp = hp['base_stat']
+        self_weight = p_pokemon['weight']
+        self_height = p_pokemon['height']
+        print("Player Pokemon")
+        print(p_pokemon['name'])
+        print("Height:")
+        print(self_height)
+        print("Weight:")
+        print(self_weight)
+        print("Speed:")
+        print(self_speed)
+        print("Heath:")
+        print(self_hp)
+
+        computer_pokemon = random.randint(1, 151)
+        url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(computer_pokemon)
+        response = requests.get(url)
+        c_pokemon = response.json()
+        stat_dic = c_pokemon['stats']
+        speed = stat_dic[int('0')]
+        hp = stat_dic[int('5')]
+        other_speed = speed['base_stat']
+        other_hp = hp['base_stat']
+        other_weight = c_pokemon['weight']
+        other_height = c_pokemon['height']
+        print(" ")
+        print("Computer Pokemon")
+        print(c_pokemon['name'])
+
+        stat=input("Select Stat" )
+        if stat == "hp":
+            if self_hp >= other_hp:
+                print (self_hp)
+                print(other_hp)
+                print("Its super effective")
+                restart = input("Next Round?" )
+            else:
+                print (self_hp)
+                print(other_hp)
+                print("Your pokemon fainted")
+                restart = input("Next Round?" )
+        elif stat == "speed":
+            if self_speed >= other_speed:
+                print(self_speed)
+                print(other_speed)
+                print("Its super effective")
+                restart = input("Next Round?")
+            else:
+                print(self_speed)
+                print(other_speed)
+                print("Your pokemon fainted")
+                restart = input("Next Round?")
+        elif stat == "weight":
+            if self_weight >= other_weight:
+                print(self_weight)
+                print(other_weight)
+                print("Its super effective")
+                restart = input("Next Round?")
+            else:
+                print(self_weight)
+                print(other_weight)
+                print("Your pokemon fainted")
+                restart = input("Next Round?")
+        elif stat == "height":
+
+            if self_height >= other_height:
+                print(self_height)
+                print(other_height)
+                print("Its super effective")
+                restart = input("Next Round?")
+            else:
+                print(self_height)
+                print(other_height)
+                print("Your pokemon fainted")
+                restart = input("Next Round?")
         else:
             print("Error")
     else:
